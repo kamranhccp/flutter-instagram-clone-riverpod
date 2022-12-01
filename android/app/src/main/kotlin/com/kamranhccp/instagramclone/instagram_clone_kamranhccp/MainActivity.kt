a7f8be4d0b54751e2774bf4433d72491cb18115a
Kamran Mansoor <kamranmansoor36@gmail.com>
2022-12-01 07:55:01 +0500

    First Step Commit

---
+++ android/app/src/main/kotlin/com/kamranhccp/instagramclone/instagram_clone_kamranhccp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kamranhccp.instagramclone.instagram_clone_kamranhccp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
